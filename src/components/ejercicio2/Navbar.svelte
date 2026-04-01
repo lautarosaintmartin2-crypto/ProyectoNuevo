8a5b164f0554afc33125be197c651bb526cb1bca
Lauti <lautarosaintmartin2@gmail.com>
2026-03-31 21:43:53 -0300

    Add Navbar component and update layout references; create Component for Ejercicio 3

--- src/components/ejercicio2/Navbar.svelte
+++
@@ -1,4 +0,0 @@
-<nav class="bg-blue-900 text-white p-4">
-    <a class="font-bold underline m-2" href="/">Ejercicio 1</a>
-    <a class="font-bold underline" href="/ej2">Ejercicio 2</a>
-</nav>
---
+++ src/components/ejercicio3/Component.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">    
+    import { email } from 'astro:schema';
+import { onMount } from 'svelte';
+    let usuarios = $state<any[]>([])
+
+    onMount(async () => {
+        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+        usuarios = await respuesta.json()
+    }) 
+
+</script>
+
+<table class="w-full"> 
+    <thead class="bg-gray-400 text-black">
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Username</th>
+                <th>Email</th>
+            </tr>
+    </thead>
+          
+    <tbody class="bg-gray-200 flex-1">
+        {#each usuarios as user}
+            <tr class="p-1 hover:bg-gray-300">
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+            </tr>
+        {/each} 
+    </tbody>
+</table>
